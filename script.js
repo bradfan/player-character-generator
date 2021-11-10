1ff382db912fce262b9968fd89f175d0d927f3f2
Brad <brt.jst@gmail.com>
2021-11-10 11:10:58 -0700

    render input and submit btn to nameBox script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 const bellCurve = () => Math.trunc(Math.random() * 6 + 1) * 3;
 const clickedBtn = document.querySelector(".btn-start");
+
 const generatorContainer = document.getElementById("generator");
 const infoContainer = document.getElementById("info");
 
 document.querySelector(".btn-start").addEventListener("click", function () {
   clickedBtn.classList.add("btn-start-after");
- // clears the created element upon each "click"
+  // clears the created element upon each "click"
   generatorContainer.innerHTML = "";
   let containerGen = document.createElement("form");
   const description = document.createElement("h3");
@@ -34,18 +35,49 @@ document.querySelector(".btn-start").addEventListener("click", function () {
   charisma.innerHTML = `Charisma: ${bellCurve()}`;
   attributes.append(charisma);
   let buttonDiv = document.createElement("div");
-  buttonDiv.setAttribute('class', 'confirm');
+  buttonDiv.setAttribute("class", "confirm");
   generatorContainer.append(buttonDiv);
   let button = document.createElement("button");
-  button.innerHTML = "If you like this character please click here; otherwise hit refresh."
-  button.name = "confirm";
-  button.id="confirm";
-  // button.setAttribute("class", "btn");
   button.setAttribute("class", "btn-confirm");
+  button.innerHTML =
+    "If you like this character please click here; otherwise hit refresh.";
+  button.name = "confirm";
+  button.id = "confirm";
+  // button.setAttribute("class", "btn");
+
   buttonDiv.append(button);
 
   generatorContainer.append(containerGen);
+
+  document.querySelector(".btn-confirm").addEventListener("click", function () {
+    const clickedConfirm = document.querySelector(".btn-confirm");
+    clickedConfirm.classList.add("btn-confirm-after");
+    let containerInfo = document.createElement("div");
+    containerInfo.setAttribute("class", "container-info");
+    let nameBox = document.createElement("div");
+    nameBox.setAttribute("class", "name-box");
+
+    containerInfo.append(nameBox);
+    let nameInput = document.createElement("input");
+    nameInput.setAttribute("class", "name-input");
+    nameInput.type = "text";
+    nameInput.id = "nameInput";
+    nameInput.placeholder = "Please enter name";
+    nameInput.name = "nameInput";
+    nameInput.value = "";
+    nameBox.append(nameInput);
+    let nameBtn = document.createElement("button");
+    nameBtn.setAttribute("class", "btn-name");
+    nameBtn.innerHTML = "Submit";
+    nameBtn.name = "nameSubmit";
+    nameBtn.id = "nameSubmit";
+    nameBox.append(nameBtn);
+    
+    
+
+    infoContainer.append(containerInfo);
   });
+});
 
 // use setAttribute to set classes for buttons individually. replace "nav" with "div" and change CSS to individual classes and not element tags.  push changes
 
