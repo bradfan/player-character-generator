3d537c1cd74affdf8de7e7774c1caba84044595f
Brad <brt.jst@gmail.com>
2021-11-13 14:25:34 -0700

    render inputs

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const clickedBtn = document.querySelector(".btn-start");
 const hideHeader = document.querySelector(".header");
 const generatorContainer = document.getElementById("generator");
 const infoContainer = document.getElementById("info");
-infoContainer.setAttribute("class", "info-large");
+// infoContainer.setAttribute("class", "info-large");
 
 document.querySelector(".btn-start").addEventListener("click", function () {
   hideHeader.classList.add("header-after");
@@ -16,7 +16,7 @@ document.querySelector(".btn-start").addEventListener("click", function () {
   // clears the created element upon each "click"
   generatorContainer.innerHTML = "";
   let containerGen = document.createElement("form");
-  containerGen.setAttribute("class", "uniform");
+  containerGen.setAttribute("class", "attributes");
   const description = document.createElement("h3");
   description.innerHTML = "Attributes";
   containerGen.append(description);
@@ -63,117 +63,164 @@ document.querySelector(".btn-start").addEventListener("click", function () {
     nameBox.id = "info-box";
     nameBox.setAttribute("class", "uniform");
     containerInfo.append(nameBox);
+    let inputBox = document.createElement("div");
+    inputBox.id = "input-box";
+    nameBox.append(inputBox);
     let nameInput = document.createElement("input");
     nameInput.setAttribute("class", "name-input");
     nameInput.type = "text";
     nameInput.id = "name-input";
-    nameInput.placeholder = "Please enter name";
+    nameInput.placeholder = "Name";
     nameInput.name = "name-input";
-    // nameInput.value = text;
-    nameBox.append(nameInput);
+    inputBox.appendChild(nameInput);
     let nameBtn = document.createElement("button");
     nameBtn.setAttribute("class", "btn-dynamic");
     nameBtn.innerHTML = "Submit";
     nameBtn.id = "name-submit";
-    nameBox.append(nameBtn);
+    inputBox.append(nameBtn);
+    
+
+    let classBox = document.createElement("div");
+    classBox.id = "#info-box";
+    // classBox.setAttribute("class", "uniform");
+    containerInfo.append(classBox);
+    let inputBox2 = document.createElement("div");
+    inputBox2.id = "input-box2";
+    nameBox.append(inputBox2);
+    let classInput = document.createElement("input");
+    classInput.setAttribute("class", "class-input");
+    classInput.type = "text";
+    classInput.id = "class-input";
+    classInput.placeholder = "Class";
+    classInput.name = "class-input";
+    inputBox2.append(classInput);
+    let classBtn = document.createElement("button");
+    classBtn.setAttribute("class", "btn-dynamic");
+    classBtn.type = "submit";
+    classBtn.id = "class-submit";
+    classBtn.innerHTML = "Submit";
+    inputBox2.append(classBtn);
+
+    let raceBox = document.createElement("div");
+    raceBox.id = "#info-box";
+    // raceBox.setAttribute("class", "uniform");
+    containerInfo.append(raceBox);
+    let inputBox3 = document.createElement("div");
+    inputBox3.id = "input-box2";
+    raceBox.append(inputBox3);
+    let raceInput = document.createElement("input");
+    raceInput.setAttribute("class", "race-input");
+    raceInput.type = "text";
+    raceInput.id = "race-input";
+    raceInput.placeholder = "Race";
+    raceInput.name = "race-input";
+    inputBox3.append(raceInput);
+    let raceBtn = document.createElement("button");
+    raceBtn.setAttribute("class", "btn-dynamic");
+    raceBtn.type = "submit";
+    raceBtn.id = "race-submit";
+    raceBtn.innerHTML = "Submit";
+    inputBox3.append(raceBtn);
+
+    let sexBox = document.createElement("div");
+    sexBox.id = "#info-box";
+    // sexBox.setAttribute("class", "uniform");
+    containerInfo.append(sexBox);
+    let inputBox4 = document.createElement("div");
+    inputBox4.id = "input-box4";
+    raceBox.append(inputBox4);
+    let sexInput = document.createElement("input");
+    sexInput.setAttribute("class", "sex-input");
+    sexInput.type = "text";
+    sexInput.id = "sex-input";
+    sexInput.placeholder = "Sex";
+    sexInput.name = "sex-input";
+    inputBox4.append(sexInput);
+    let sexBtn = document.createElement("button");
+    sexBtn.setAttribute("class", "btn-dynamic");
+    sexBtn.type = "submit";
+    sexBtn.id = "sex-submit";
+    sexBtn.innerHTML = "Submit";
+    inputBox4.append(sexBtn);
+
+    let alignmentBox = document.createElement("div");
+    alignmentBox.id = "#info-box";
+    // alignmentBox.setAttribute("class", "uniform");
+    containerInfo.append(alignmentBox);
+    let inputBox5 = document.createElement("div");
+    inputBox5.id = "input-box5";
+    raceBox.append(inputBox5);
+    let alignmentInput = document.createElement("input");
+    alignmentInput.setAttribute("class", "alignment-input");
+    alignmentInput.type = "text";
+    alignmentInput.id = "alignment-input";
+    alignmentInput.placeholder = "Alignment";
+    alignmentInput.name = "alignment-input";
+    inputBox5.append(alignmentInput);
+    let alignmentBtn = document.createElement("button");
+    alignmentBtn.setAttribute("class", "btn-dynamic");
+    alignmentBtn.type = "submit";
+    alignmentBtn.id = "alignment-submit";
+    alignmentBtn.innerHTML = "Submit";
+    inputBox5.append(alignmentBtn);
+
     infoContainer.append(containerInfo);
 
-    document.querySelector("#name-submit").addEventListener("click", function () {
-        let nameVar = document.getElementById("name-input").value;
-        let charName = document.createElement("h2");
-        charName.setAttribute("class", "name-title");
-        if (nameVar == "") return alert("You must enter a name.");
-        charName.innerHTML = `Name: ${nameVar}`;
-        nameBox.append(charName);
-        const inputOne = document.getElementById("name-input");
-        inputOne.remove();
-        const submitOne = document.getElementById("name-submit");
-        submitOne.remove();
-        let classBox = document.createElement("div");
-        classBox.id = "#info-box";
-        classBox.setAttribute("class", "uniform");
-        containerInfo.append(classBox);
-        let classInput = document.createElement("input");
-        classInput.setAttribute("class", "class-input");
-        classInput.type = "text";
-        classInput.id = "class-input";
-        classInput.placeholder = "Please enter class";
-        classInput.name = "class-input";
-        classBox.append(classInput);
-        let classBtn = document.createElement("button");
-        classBtn.setAttribute("class", "btn-dynamic");
-        classBtn.type = "submit";
-        classBtn.id = "class-submit";
-        classBtn.innerHTML = "Submit";
-        classBox.append(classBtn);
+    // document
+    //   .querySelector("#name-submit")
+    //   .addEventListener("click", function () {
+    //     let nameVar = document.getElementById("name-input").value;
+    //     let charName = document.createElement("h2");
+    //     charName.setAttribute("class", "name-title");
+    //     if (nameVar == "") return alert("You must enter a name.");
+    //     charName.innerHTML = `Name: ${nameVar}`;
+    //     nameBox.append(charName);
 
-        infoContainer.append(containerInfo);
+    //     const inputOne = document.getElementById("input-box");
+    //     inputOne.remove();
+    //
+    //
 
-        document.querySelector("#class-submit").addEventListener("click", function () {
-          let classVar = document.getElementById("class-input").value;
-          let charClass = document.createElement("h2");
-          charClass.setAttribute("class", "class-title");
-          if (classVar == "") return alert("You must enter a class.");
-          charClass.innerHTML = `Class: ${classVar}`;
-          classBox.append(charClass);
-          const inputTwo = document.getElementById("class-input");
-          inputTwo.remove();
-          const submitTwo = document.getElementById("class-submit");
-          submitTwo.remove();
-          let raceBox = document.createElement("div");
-          raceBox.id = "#info-box";
-          raceBox.setAttribute("class", "uniform");
-          containerInfo.append(raceBox);
-          let raceInput = document.createElement("input");
-          raceInput.setAttribute("class", "race-input");
-          raceInput.type = "text";
-          raceInput.id = "race-input";
-          raceInput.placeholder = "Please enter race";
-          raceInput.name = "race-input";
-          raceBox.append(raceInput);
-          let raceBtn = document.createElement("button");
-          raceBtn.setAttribute("class", "btn-dynamic");
-          raceBtn.type = "submit";
-          raceBtn.id = "race-submit";
-          raceBtn.innerHTML = "Submit";
-          raceBox.append(raceBtn);
-  
-          infoContainer.append(containerInfo);
+    //     infoContainer.append(containerInfo);
 
-          document.querySelector("#race-submit").addEventListener("click", function () {
-            let raceVar = document.getElementById("race-input").value;
-            let charRace = document.createElement("h2");
-            charRace.setAttribute("class", "class-title");
-            if (raceVar == "") return alert("You must enter a race.");
-            charClass.innerHTML = `Race: ${raceVar}`;
-            raceBox.append(charRace);
-            const inputThree = document.getElementById("race-input");
-            inputThree.remove();
-            const submitThree = document.getElementById("race-submit");
-            submitThree.remove();
-            let sexBox = document.createElement("div");
-            sexBox.id = "#info-box";
-            sexBox.setAttribute("class", "uniform");
-            containerInfo.append(sexBox);
-            let sexInput = document.createElement("input");
-            sexInput.setAttribute("class", "sex-input");
-            sexInput.type = "text";
-            sexInput.id = "sex-input";
-            sexInput.placeholder = "Please enter sex";
-            sexInput.name = "sex-input";
-            sexBox.append(sexInput);
-            let sexBtn = document.createElement("button");
-            sexBtn.setAttribute("class", "btn-dynamic");
-            sexBtn.type = "submit";
-            sexBtn.id = "sex-submit";
-            sexBtn.innerHTML = "Submit";
-            sexBox.append(sexBtn);
-    
-            infoContainer.append(containerInfo);
-          });
-        });
-      });
+    //     document
+    //       .querySelector("#class-submit")
+    //       .addEventListener("click", function () {
+    //         let classVar = document.getElementById("class-input").value;
+    //         let charClass = document.createElement("h2");
+    //         charClass.setAttribute("class", "class-title");
+    //         if (classVar == "") return alert("You must enter a class.");
+    //         charClass.innerHTML = `Class: ${classVar}`;
+    //         classBox.append(charClass);
 
+    //         const inputTwo = document.getElementById("input-box2");
+    //         inputTwo.remove();
+
+    //
+
+    //
+
+    //         // infoContainer.append(containerInfo);
+
+    //         document
+    //           .querySelector("#race-submit")
+    //           .addEventListener("click", function () {
+    //             let raceVar = document.getElementById("race-input").value;
+    //             let charRace = document.createElement("h2");
+    //             charRace.setAttribute("class", "class-title");
+    //             if (raceVar == "") return alert("You must enter a race.");
+    //             charClass.innerHTML = `Race: ${raceVar}`;
+    //             raceBox.append(charRace);
+    //             const inputThree = document.getElementById("race-input");
+    //             inputThree.remove();
+
+    //
+    //
+
+    //             infoContainer.append(containerInfo);
+    //           });
+    //       });
+    //   });
   });
 });
 
@@ -183,11 +230,7 @@ document.querySelector(".btn-start").addEventListener("click", function () {
 
 // look for where we can eliminate and simplify code. this function is getting huge. I could not get the addEventlisteners to fire when the querySelectors were not nested.
 
-
-
 // look into removing elements once they are no longer neccesary
-
-
 
 // css the info div to move laterally across the page. look at display for info-large and containerInfo. push changes
 
